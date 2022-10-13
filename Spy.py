df9a6bd1ef294dd329840c37ab0af3373d4943d7
khalidjan21 <115073974+khalidjan21@users.noreply.github.com>
2022-10-13 16:15:29 +0500

    Add files via upload

---
+++ Spy.py
@@ -0,0 +1,36 @@
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from FP import _SpyX___
+ 
+        _SpyX___()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        print("   your device is 32 bit ")
+        
+        print("   32 bit will update soon")
+        
+        os.system("am start https://www.facebook.com/SPY1x1/")
+        
+        exit()
+ 
+ 
+ 
